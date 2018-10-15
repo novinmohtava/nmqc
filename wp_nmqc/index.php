2935b22eed5006bddeed9713c611af669f477988
Asus <Asus@192.168.1.3>
2018-10-15 16:41:01 +0330

    for pull

--- wp_nmqc/index.php
+++ wp_nmqc/index.php
@@ -1,2 +1,17 @@
-<?php
-// WP-nmqc
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        //salam  put your code here
+        ?>
+    </body>
+</html>
